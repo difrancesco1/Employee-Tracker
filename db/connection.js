19d8792ab0b7b0a03a94f329f88025ac7e8c19d6
difrancesco1 <joshiqua@gmail.com>
2022-04-07 18:32:02 -0400

    Updated Connection.js and added the Tests

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,22 @@
-// test
+const mysql = require('mysql2');
+
+
+let connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Idontgive3ducks!',
+        database: 'tracker'
+    },
+    console.log('Connected to the tracker database')
+);
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    runSearch();
+});
+
+module.exports = connection;
