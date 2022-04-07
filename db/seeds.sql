b7be2a9f86310581a8cb03c934a56d357ba0509a
difrancesco1 <joshiqua@gmail.com>
2022-04-07 17:59:11 -0400

    DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO departments (name) VALUES ("UX");
+INSERT INTO departments (name) VALUES ("Engineering");
+INSERT INTO departments (name) VALUES ("Manager");
+INSERT INTO departments (name) VALUES ("IT Support");
+
+
+INSERT INTO roles (title, salary, department_id) VALUES ("UX Engineer","120000","1");
+INSERT INTO roles (title, salary, department_id) VALUES ("Software Engineer","120000","2");
+INSERT INTO roles (title, salary, department_id) VALUES ("Manager","105000","3");
+INSERT INTO roles (title, salary, department_id) VALUES ("IT Support","45500","4");
+
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Josh", "DiFrancesco", 1);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Linh", "Ho", 2);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Alex", "Mass", 3);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Jenny", "Star", 4);
